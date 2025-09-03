35c4f51b14f3d7faea286008fda8b9fd1331c6dc
Joao Pedro <engjpsoft@gmail.com>
2025-09-03 17:41:30 -0300

    Update SecurityConfig.java

--- neoapp/src/main/java/com/neoapp/clienteapi/security/SecurityConfig.java
+++ neoapp/src/main/java/com/neoapp/clienteapi/security/SecurityConfig.java
@@ -4,46 +4,44 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.authentication.AuthenticationManager;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
     private final UserDetailsServiceImpl userDetailsService;
 
-    public SecurityConfig(JwtProvider jwtProvider, UserDetailsServiceImpl userDetailsService) {
+    public SecurityConfig (JwtProvider jwtProvider, UserDetailsServiceImpl userDetails) {
         this.jwtProvider = jwtProvider;
-        this.userDetailsService = userDetailsService;
+        this.userDetailsService = userDetails;
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean 
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
         JwtAuthFilter jwtFilter = new JwtAuthFilter(jwtProvider, userDetailsService);
 
         http
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/auth/**").permitAll()  
-                    .requestMatchers("/api/clientes/**").authenticated() 
+                    .requestMatchers("/api/clientes/**").authenticated()    
+                    .anyRequest().permitAll()
             )
-            .userDetailsService(userDetailsService)  
             .formLogin(form -> form.disable())
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
+            
         return http.build();
-    }
 
+    }
+    
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    public AuthenticationManager AuthenticationManager (AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
+
+
 }
-
-
