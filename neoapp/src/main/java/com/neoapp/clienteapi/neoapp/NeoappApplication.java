ef0b81537e2b0949dab8203f92aa2dbb4565c524
Joao Pedro <engjpsoft@gmail.com>
2025-09-03 15:00:07 -0300

    Update NeoappApplication.java

--- neoapp/src/main/java/com/neoapp/clienteapi/neoapp/NeoappApplication.java
+++ neoapp/src/main/java/com/neoapp/clienteapi/neoapp/NeoappApplication.java
@@ -1,8 +1,13 @@
 package com.neoapp.clienteapi.neoapp;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@ComponentScan(basePackages = {
+    "com.neoapp.clienteapi.neoapp",
+    "com.neoapp.clienteapi.security"
+})
 @SpringBootApplication
 public class NeoappApplication {
 
