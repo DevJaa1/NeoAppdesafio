661eeabb6040d2e93dfa24406c57969ad202bbaa
Joao Pedro <engjpsoft@gmail.com>
2025-09-03 23:09:50 -0300

    Update SecurityConfig.java

--- neoapp/src/main/java/com/neoapp/clienteapi/neoapp/security/SecurityConfig.java
+++ neoapp/src/main/java/com/neoapp/clienteapi/neoapp/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/clientes/**").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().permitAll()
             )
             .formLogin(form -> form.disable())
